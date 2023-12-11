22118aef0866988eca5a5fc38730d713ce4290a8
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-12-11 12:16:17 +0100

    setup lev 1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- Navbar Menu -->
+    <div class="p-6 bg-blue-500 text-white">
+      <p>Navbar Menu</p>
+      <!-- Placez ici le contenu de votre Navbar -->
+    </div>
+
+    <!-- En-tête (Hero) -->
+    <div class="p-6 bg-green-500 text-white flex-grow">
+      <p>En-tête (Hero)</p>
+      <!-- Contenu de votre En-tête -->
+    </div>
+
+    <!-- Liste de produits (ProductList) -->
+    <div class="p-6 bg-red-500 text-white flex-grow">
+      <p>Liste de produits</p>
+      <!-- Contenu de votre liste de produits -->
+    </div>
+
+    <!-- Pied de page (Footer) -->
+    <div class="p-6 bg-yellow-500 text-white mt-auto">
+      <p>Pied de page</p>
+      <!-- Placez ici le contenu de votre Footer -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style></style>
